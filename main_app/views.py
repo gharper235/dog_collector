03caf3e79a1e049e8c10afe172d429fbfdd22e76
Greg Harper <gharper235@gmail.com>
2021-04-14 11:21:53 -0500

    got an about url

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,5 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+def about(request):
+  return HttpResponse('<h1>About the Dog Collector</h1>')
