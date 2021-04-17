69504e8c1646c29196e16cc57efcebd87ef30058
Greg Harper <gharper235@gmail.com>
2021-04-17 13:36:18 -0500

    completed day 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,3 +12,6 @@ class Dog(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Create your views here.
@@ -23,3 +24,19 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
+
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+
+
+class DogUpdate(UpdateView):
+    model = Dog
+    # Disallow the renaming of a dog by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
