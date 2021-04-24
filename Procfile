274e24f48e3666a56ad791ae84b955ba9ad478a6
Greg Harper <gharper235@gmail.com>
2021-04-24 15:17:46 -0500

    Adds heroku deployment config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dog_collector.wsgi
--- dogcollector_project/settings.py
+++ dogcollector_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from decouple import config
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,4 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals())
