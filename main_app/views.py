40b659d424eae86e91cc2249bca602d0fdedef7f
Greg Harper <gharper235@gmail.com>
2021-04-24 14:51:47 -0500

    through step 9 and working

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 import uuid
 import boto3
 from .models import Dog, Food, Photo
@@ -48,6 +50,12 @@ def add_walking(request, dog_id):
 class DogCreate(CreateView):
     model = Dog
     fields = ['name', 'breed', 'description', 'age']
+
+    def form_valid(self, form):
+        # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the cat
+        # Let the CreateView do its job as usual
+        return super().form_valid(form)
 
 
 class DogUpdate(UpdateView):
@@ -124,3 +132,23 @@ def add_photo(request, dog_id):
         except:
             print('An error occurred uploading file to S3')
     return redirect('detail', dog_id=dog_id)
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
